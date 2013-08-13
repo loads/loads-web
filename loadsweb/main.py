837fdc9b5e5e1ed1e8103b2daa1a2f51ee05cc9e
Tarek Ziade <tarek@ziade.org>
2013-08-13 11:04:25 +0200

    added a base template

--- loadsweb/main.py
+++ loadsweb/main.py
@@ -2,16 +2,18 @@ import sys
 import os
 
 from bottle import route, run, SimpleTemplate
-from bottle import app as _app
+from bottle import app as _app, TEMPLATE_PATH
 
 from loadsweb.controller import Controller
 
 _TMPL = os.path.join(os.path.dirname(__file__), 'templates')
+TEMPLATE_PATH.append(_TMPL)
 
 
 def render(name, **options):
     with open(os.path.join(_TMPL, name + '.tmpl')) as f:
-        return SimpleTemplate(f.read()).render(**options)
+
+        return SimpleTemplate(f.read(), lookup=[_TMPL]).render(**options)
 
 
 @route('/')
