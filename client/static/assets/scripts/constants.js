fa1372e20b1d60b06d330e3be675c172be6b4c6c
David Walsh <davidwalsh83@gmail.com>
2015-03-10 13:37:22 -0500

    Adding first steps of project loader

--- client/static/assets/scripts/constants.js
+++ client/static/assets/scripts/constants.js
@@ -8,6 +8,7 @@ angular.module('LoadsApp')
     AGENTS_CHECK: '/#/agents/check',
     PROJECTS_LISTING: '/#/projects',
     PROJECTS_BUILDER: '/#/projects-builder',
+    PROJECTS_LOADER: '/#/projects-loader',
     RUNS: '/#/runs',
     RUNS_ACTIVE: '/#/runs/active',
     RUNS_FINISHED: '/#/runs/finished',
