712c007692bcd7ceea4db3fde57b4c63b9a8f53a
David Walsh <davidwalsh83@gmail.com>
2015-03-12 15:35:57 -0500

    Fixing whitespace issues.

--- server/routes/gist.js
+++ server/routes/gist.js
@@ -4,34 +4,34 @@ module.exports = [{
   method: 'GET',
   path: '/api/gist/{id}',
   handler: function(request, reply) {
-	var id = request.params.id;
-	var output = {};
+    var id = request.params.id;
+    var output = {};
 
-	require('octonode').client().gist().get(id, function(err, data) {
-		if(err) { // Basic error
-			output = err;
-			output.success = false;
-		}
-		else {
-			output.owner = data.owner.login;
-			output.owner_avatar_url = data.owner.avatar_url;
-			output.success = true;
+    require('octonode').client().gist().get(id, function(err, data) {
+      if(err) { // Basic error
+        output = err;
+        output.success = false;
+      }
+      else {
+        output.owner = data.owner.login;
+        output.owner_avatar_url = data.owner.avatar_url;
+        output.success = true;
 
-			['description', 'comments', 'created_at', 'update_at', 'id', 'public'].forEach(function(i) {
-				output[i] = data[i];
-			});
+        ['description', 'comments', 'created_at', 'update_at', 'id', 'public'].forEach(function(i) {
+          output[i] = data[i];
+        });
 
-			var files = output.files = extractGistFiles(data.files);
+        output.files = extractGistFiles(data.files);
 
-                  // If the files are no good, go back to error mode
-                  if(!files.length || files[0].content.error) {
-                    output.success = false;
-                    output.error = 'Invalid JSON:  File could not be parsed.';
-                  }
-		}
-		
-		reply(JSON.stringify(output, null, 2));
-	});	
+        // If the files are no good, go back to error mode
+        if(!output.files.length || output.files[0].content.error) {
+          output.success = false;
+          output.error = 'Invalid JSON:  File could not be parsed.';
+        }
+      }
+
+      reply(JSON.stringify(output, null, 2));
+    });
   },
   config: {
     description: 'API to get gists from github and return the data',
@@ -53,13 +53,13 @@ function extractGistFiles(filesObj) {
     var file = filesObj[key];
     // If the current file is a .JSON file, convert it from a string to an object.
     if (file.type === 'application/json') {
-    	try {
-    		file.content = JSON.parse(file.content);
-    	}
+      try {
+        file.content = JSON.parse(file.content);
+      }
       catch(e) {
-      	file.content = {
-      		error: 'File contents could not be parsed'
-      	};
+        file.content = {
+          error: 'File contents could not be parsed'
+        };
       }
     }
     return file;
